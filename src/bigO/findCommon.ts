94ce754935a1677a7535a18d480cf1ee661b487a
girage <45748977+girage@users.noreply.github.com>
2022-03-12 19:15:10 +0700

    add Best  approach

--- src/bigO/findCommon.ts
+++ src/bigO/findCommon.ts
@@ -2,9 +2,14 @@ const array1: Array<string> = ['a', 'b', 'c', 'x'];
 const array2: Array<string> = ['z', 'y', 'i'];
 const array3: Array<string> = ['z', 'y', 'x'];
 
+/* 
+My approach 
+Time complexity: O(a+b)
+Space complexity: O(a+b)
+*/
 const checkCommon = (arr1: Array<string>, arr2: Array<string>): Boolean => {
-  let setA: Set<string> = new Set();
-  let setB: Set<string> = new Set();
+  const setA: Set<string> = new Set();
+  const setB: Set<string> = new Set();
   let setCommon: Set<string> = new Set();
 
   arr1.forEach((element: string): void => {
@@ -20,9 +25,21 @@ const checkCommon = (arr1: Array<string>, arr2: Array<string>): Boolean => {
       setCommon.add(element);
     }
   });
-  console.log(setCommon);
+
   return setCommon.size > 0;
 }
 
-console.log(checkCommon(array1, array2));
-console.log(checkCommon(array1, array3));
+// console.log(checkCommon(array1, array2));
+// console.log(checkCommon(array1, array3));
+
+/* 
+Best  approach 
+Time complexity: O(a+b)
+Space complexity: O(a)
+*/
+
+const containCommonItem = (arr1: Array<string>, arr2: Array<string>):Boolean => {
+  return arr1.some((item: string) => arr2.includes(item));
+}
+
+// console.log(containCommonItem(array1, array3));
